9135a804a0e6b3f2c2e27053f4d78e7c7f0d2c2e
NikitaFedkushov <fedkushov@gmail.com>
2024-07-21 18:09:58 +0300

    add Lesson 1 Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 767687;
+int b = 8;
+int c = 345;
+int d = 342;
+int e = 2365;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
+
